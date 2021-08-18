e16440aa5aefe34f1e92761f2bd5dca37c31a46a
Carlos Manuel <chars.dev@gmail.com>
2021-08-18 12:31:12 -0500

    Add form to add events

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,46 @@
 <template>
-  <app-header></app-header>
+  <AppHeader @exportFile="exportFileTo" />
+
+  <div class="content">
+    <aside class="aside">
+      <EventForm @addNewEvent="addNewEvent" />
+    </aside>
+
+    <main class="main">Timeline content</main>
+  </div>
 </template>
 
 <script>
 import AppHeader from "./header/AppHeader.vue";
+import EventForm from "./events/EventForm.vue";
 import "boxicons/css/boxicons.min.css";
 
 export default {
   name: "App",
   components: {
     AppHeader,
+    EventForm,
+  },
+  methods: {
+    addNewEvent(newEvent) {
+      console.log(newEvent);
+    },
   },
 };
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
 body {
-  min-width: 90%;
+  max-width: 90%;
+}
+
+.content {
+  display: grid;
+  grid-template-columns: 300px 1fr;
+  gap: 15px;
 }
 </style>
---
+++ src/events/EventForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <form @submit.prevent="handleSubmit" class="event-form">
+    <input type="text" placeholder="Event name" v-model="event.name" />
+    <input type="date" v-model="event.date" />
+    <textarea placeholder="Description" v-model="event.description"></textarea>
+    <input type="url" placeholder="Image url" v-model="event.image" />
+
+    <button>
+      <i class="bx bx-message-alt-add"></i>
+      ADD EVENT
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "EventForm",
+  data() {
+    return {
+      event: {
+        name: "",
+        description: "",
+        date: null,
+        image: "",
+      },
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.$emit("addNewEvent", this.event);
+      this.event = { name: "", description: "", date: null, image: "" };
+    },
+  },
+};
+</script>
+
+<style scoped>
+input,
+textarea,
+button {
+  width: 100%;
+  margin: 5px;
+}
+</style>
