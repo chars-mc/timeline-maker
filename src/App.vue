493467e292673cc6ad11e3742c036a160dc8686b
Carlos Manuel <chars.dev@gmail.com>
2021-08-17 20:10:50 -0500

    Remove unused components and add Header

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <app-header></app-header>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import AppHeader from "./header/AppHeader.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    AppHeader,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/header/AppHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Timeline Maker</h1>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
