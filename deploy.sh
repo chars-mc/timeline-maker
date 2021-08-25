03a5444dc2648408de0dda5dc3fcfc2c2571f762
Carlos Manuel <chars.dev@gmail.com>
2021-08-25 16:44:28 -0500

    Add deploy config

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f https://github.com/chars-mc/timeline-maker.git main:gh-pages
+
+cd -
